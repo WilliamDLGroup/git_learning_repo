7678ab6fdc0f68c2c94ff641d15ccd36341508b8
liuliang1356 <2792071712@qq.com>
2023-12-05 15:46:19 +0800

    add @rollup/plugin-html  plugin to project

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,14 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss'
+
+import html from '@rollup/plugin-html';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
+
 
 export default {
 	input: 'src/main.js',
@@ -14,8 +18,13 @@ export default {
 		sourcemap: true
 	},
 	plugins: [
+		postcss({
+			extract: 'bundle.css',
+			plugins: []
+		}),
 		resolve(), // tells Rollup how to find date-fns in node_modules
 		commonjs(), // converts date-fns to ES modules
-		production && terser() // minify, but only in production
+		html(),
+		production && terser(), // minify, but only in production,
 	]
 };
